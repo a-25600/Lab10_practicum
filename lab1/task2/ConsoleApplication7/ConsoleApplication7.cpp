843cc4192005474ba27ad1e3884ccdc0378cb9da
a-25600 <aleksagunya56@gmail.com>
2025-11-15 14:35:47 +0200

    Add lab1 task2

---
+++ lab1/task2/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "Windows.h"
+using namespace System;
+int main(array<System::String^>^ args)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Console::WriteLine(L"Введіть перший рядок: ");
+	String^ a = Console::ReadLine();
+	Console::WriteLine(L"Введіть другий рядок: ");
+	String^ b = Console::ReadLine();
+	String^ c(a + b);
+	Console::WriteLine(c);
+	return 0;
+}
