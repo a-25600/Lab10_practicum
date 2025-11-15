18eea529ac5941b3fb38a55009ed97aea99296e4
a-25600 <aleksagunya56@gmail.com>
2025-11-15 14:28:30 +0200

    Add lab1 task1

---
+++ lab1/task1/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "Windows.h"
+using namespace System;
+int main(array<System::String^>^ args)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Console::WriteLine(L"Введіть рядок: ");
+	String^ a = Console::ReadLine();
+	String^ b(L"begin " + a + L" end");
+	Console::WriteLine(b);
+	Console::WriteLine("Рядок містить {0} символів.", b->Length);
+	return 0;
+}
+
