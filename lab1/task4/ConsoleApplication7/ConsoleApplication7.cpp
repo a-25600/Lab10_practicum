4cafc4ce3dfd366aa34837382d3a22b47d1be31a
a-25600 <aleksagunya56@gmail.com>
2025-11-15 14:43:53 +0200

    Add lab1 task4

---
+++ lab1/task4/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "Windows.h"
+using namespace System;
+int main(array<System::String^>^ args)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Console::WriteLine(L"Введіть рядок: ");
+	String^ a = Console::ReadLine();
+	String^ b(L"");
+	for (int i = a->Length - 1; i >= 0; i--)
+		b += a[i];
+	Console::WriteLine(b);
+	return 0;
+}
