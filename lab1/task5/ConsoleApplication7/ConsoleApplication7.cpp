33f7c3717f3aeb3b5550f63db4b7e785e6bc2653
a-25600 <aleksagunya56@gmail.com>
2025-11-15 14:45:47 +0200

    Add lab1 task5

---
+++ lab1/task5/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "Windows.h"
+using namespace System;
+int main(array<System::String^>^ args)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Console::WriteLine(L"Введіть перший рядок: ");
+	String^ a = Console::ReadLine();
+	Console::WriteLine(L"Введіть другий рядок: ");
+	String^ b = Console::ReadLine();
+	if (a->Length < b->Length)
+		Console::WriteLine(L"{0} менше, ніж {1}.", a, b);
+	else if (a->Length > b->Length)
+		Console::WriteLine(L"{0} більше, ніж {1}.", a, b);
+	else
+		Console::WriteLine(L"{0} еквівалентно {1}.", a, b);
+	return 0;
+}
